ad4c2f016e519be99ac1eef3b121e62f2398bcab
benballintyn <bbal@brandeis.edu>
2020-01-29 12:09:30 -0500

    updated EVLIFnetwork_test.m

--- test/EVLIFnetwork_test.m
+++ test/EVLIFnetwork_test.m
@@ -6,10 +6,10 @@ net.addGroup('group2',1000,'excitatory',2);
 net.addGroup('group3',1000,'excitatory',3);
 net.addGroup('group4',1000,'inhibitory',4);
 
-weightRange = 0:1e-15:1.1e-10;
+weightRange = 0:1e-12:10e-8;
 uniformWeightDist = weightDistribution(weightRange,ones(1,length(weightRange))*(1/length(weightRange)));
-randConnProb = .01;
-weightFunction = @(x) 1.1e-10*rand(size(x));
+randConnProb = .1;
+weightFunction = @(x) 1e-8*rand(size(x));
 gaussConnProbFunction = @(x)(sqrt(2)/(.05*sqrt(pi)))*exp(-x.^2/(2*.05^2));
 
 gaussConnParams.connProbFunction = gaussConnProbFunction;
@@ -33,54 +33,34 @@ net.connect(4,1,'random',randConnParams2);
 net.connect(4,2,'random',randConnParams2);
 net.connect(4,3,'random',randConnParams2);
 
-useGpu = 1;
+weightRange = 0:1e-12:4.5e-8;
+uniformWeightDist = weightDistribution(weightRange,ones(1,length(weightRange))*(1/length(weightRange)));
+spkInputParams.connProb = .05;
+spkInputParams.weightDistribution = uniformWeightDist;
+net.addSpikeGenerator('spk1',1000,'excitatory',10);
+net.connect(-1,1,'random',spkInputParams);
+
+useGpu = 0;
 disp(['useGPU = ' num2str(useGpu)])
-dt=1e-4;
-simTime = 1;
-if (useGpu)
-    %allSpikes = gpuArray(zeros(net.nNeurons,nT,'single'));
-    simobj.dt = single(dt);
-else
-    %allSpikes = zeros(net.nNeurons,nT);
-    simobj.dt = dt;
-end
-nT = ceil((simTime/dt));
-%{
-if (useGpu)
-    allSpikes = gpuArray(zeros(net.nNeurons,nT,'single'));
-    allVs = gpuArray(zeros(net.nNeurons,nT,'single'));
-else
-    allSpikes = zeros(net.nNeurons,nT);
-    allVs = zeros(net.nNeurons,nT);
-end
-%}
+
 [V,Vreset,tau_ref,Vth,Vth0,Vth_max,VsynE,VsynI,GsynE,GsynI,maxGsynE,maxGsynI,dGsyn,tau_synE,...
           tau_synI,Cm,Gl,El,dth,Iapp,std_noise,dt,ecells,icells,spikeGenProbs,cells2record] = ...
           setupEVLIFNet(net,useGpu);
+simTime = 1;
+nT = ceil((simTime/dt));
 
-%{
-for i=1:nT
-    [V,Gref,GsynE,GsynI,spiked] = updateNet_mex(V,Gref,dGref,tau_ref,Vth,VsynE,VsynI,GsynE,GsynI,maxGsynE,maxGsynI,...
-                            dGsyn,tau_synE,tau_synI,Cm,Gl,El,Ek,dth,Iapp,dt,ecells,icells);
-    allVs(:,i) = V;
-    allSpikes(:,i) = spiked;
-    if (mod(i,1000)==0)
-        disp(num2str(i))
-    end
-end
-%}
 spkfid = fopen('test.bin','W');
 if (~useGpu)
     disp('compiling')
     compile_loopUpdateEVLIFNetCPU();
     disp('starting simulation')
     tic;
-    loopUpdateEVLIFNetCPU(V,Vreset,tau_ref,Vth,Vth0,Vth_max,VsynE,VsynI,GsynE,GsynI,maxGsynE,maxGsynI,...
+    loopUpdateEVLIFNetCPU_mex(V,Vreset,tau_ref,Vth,Vth0,Vth_max,VsynE,VsynI,GsynE,GsynI,maxGsynE,maxGsynI,...
                             dGsyn,tau_synE,tau_synI,Cm,Gl,El,dth,Iapp,std_noise,...
                             dt,ecells,icells,spikeGenProbs,cells2record,nT,spkfid);
     sim_dur = toc;
     fclose(spkfid);
-    disp(['loopUpdateEVLIFNetCPU: Total sim time: ' num2str(sim_dur) '. Time per timestep = ' num2str(sim_dur/(simTime/simobj.dt)) ' --> ' num2str((sim_dur/(simTime/simobj.dt))/simobj.dt) 'x real time'])
+    disp(['loopUpdateEVLIFNetCPU: Total sim time: ' num2str(sim_dur) '. Time per timestep = ' num2str(sim_dur/(simTime/dt)) ' --> ' num2str((sim_dur/(simTime/dt))/dt) 'x real time'])
 else
     compile_loopUpdateEVLIFNetGPU_fast(net.nNeurons,length(spikeGenProbs),length(cells2record));
     tic;
@@ -91,3 +71,9 @@ else
     fclose(spkfid);
     disp(['loopUpdateEVLIFNetGPU_fast: Total sim time: ' num2str(sim_dur) '. Time per timestep = ' num2str(sim_dur/(simTime/simobj.dt)) ' --> ' num2str((sim_dur/(simTime/simobj.dt))/simobj.dt) 'x real time'])
 end
+
+% retrieve spike data and compute firing rates
+[spikeData] = readSpikes('test.bin',cells2record);
+window = .1/dt; % 100ms
+downsampleFactor = 10;
+frs = getFiringRates(spikeData,length(cells2record),nT,dt,downsampleFactor,window);
